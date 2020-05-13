8f545c53db0c4b164fa3d36714a78258ba8bc226
Irina <iloushkina@gmail.com>
2020-05-13 16:48:22 +0200

    home task 2 done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfMovies = +prompt("How many movies have you benn watching already?", "");
 
 // create object
@@ -9,12 +11,27 @@ const personalMovieDB = {
    private: false
 };
 
-const a = prompt("Your last movie?", ""),
-   b = prompt("Rate it", ""),
-   c = prompt("Your last movie?", ""),
-   d = prompt("Rate it", "");
+// loop 
+for (let i = 0; i < 2; i++) {
+   const a = prompt("Your last movie?", ""),
+      b = prompt("Rate it", "");
+   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+   } else {
+      console.log("error");
+      i--;
+   }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+   console.log("Not too much");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+   console.log("Wow!");
+} else if (personalMovieDB.count >= 30) {
+   console.log("you are crazy, man!");
+} else {
+   console.log("there is a mistake");
+}
 
 console.log(personalMovieDB);
